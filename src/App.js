ca5680cc606a34b3becfbbeac57bccbb80aa33b2
Deepak Singh Tomar <130336302+Deepakrocknow@users.noreply.github.com>
2024-11-03 22:59:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Shop/>}/>
         <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
-        <Route path='/bikni' element={<ShopCategory banner={women_banner} category="bikni"/>}/>
+        // <Route path='/bikni' element={<ShopCategory banner={women_banner} category="bikni"/>}/>
         <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
         <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}/>
         <Route path="/product" element={<Product/>}>
